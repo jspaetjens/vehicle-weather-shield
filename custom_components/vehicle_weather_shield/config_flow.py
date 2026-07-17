37172337a82c1dee12915955afb01c2f1d639cf8
Jack Spaetjens <jack.spaetjens@vipergear.nl>
2026-07-17 08:53:43 +0200

    Add config flow

---
+++ custom_components/vehicle_weather_shield/config_flow.py
@@ -0,0 +1,45 @@
+"""Config flow for Vehicle Weather Shield."""
+
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.data_entry_flow import FlowResult
+
+from . import DOMAIN
+
+
+class VehicleWeatherShieldConfigFlow(
+    config_entries.ConfigFlow,
+    domain=DOMAIN,
+):
+    """Handle a config flow for Vehicle Weather Shield."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self,
+        user_input: dict[str, Any] | None = None,
+    ) -> FlowResult:
+        """Handle the initial setup."""
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Vehicle Weather Shield",
+                data=user_input,
+            )
+
+        schema = vol.Schema(
+            {
+                vol.Required(
+                    "vehicle_name",
+                    default="My Vehicle",
+                ): str,
+            }
+        )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+        )
